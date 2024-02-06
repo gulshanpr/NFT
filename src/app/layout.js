920804347c65de458083dbbbdc0f75e7f714524d
gulshanpr <gulshankumarprasad04@gmail.com>
2024-02-07 02:02:10 +0530

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -13,5 +14,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
+    
   );
 }
